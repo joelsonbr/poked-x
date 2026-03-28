994308b06920e8b1037bac0242e1d26edb17baf2
joelsonbr <joelsonpaz1@gmail.com>
2026-03-28 19:48:01 -0300

    feat: login e cadastro

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', // Sem o /api aqui
-})
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
-export default api
+export default api;
